241269d5fff94f2e955cec8ee97e796546ce4cb7
xueweihan <595666367@qq.com>
2021-03-05 23:19:13 +0800

    update test.sh

--- shell/test.sh
+++ shell/test.sh
@@ -4,4 +4,4 @@ set -e
 set -x
 
 #pytest -s --cov=hydra --cov-report=term-missing --cov-report=html  hydra/tests
-pytest --cov=hydra --cov-report=term-missing --ignore=main.py hydra/tests --cov-fail-under=95
+pytest --cov=hydra --ignore=main.py hydra/tests --cov-fail-under=95
