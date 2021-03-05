fa7a7498edc0db9fa98ab113f767464340cd1db4
xueweihan <595666367@qq.com>
2021-03-06 00:02:33 +0800

    add test coverage

--- shell/test.sh
+++ shell/test.sh
@@ -4,4 +4,4 @@ set -e
 set -x
 
 #pytest -s --cov=hydra --cov-report=term-missing --cov-report=html  hydra/tests
-pytest --cov=hydra --ignore=main.py hydra/tests --cov-fail-under=95
+pytest --cov=hydra --cov-report=term-missing hydra/tests --cov-fail-under=95
